73544a5f8cb81088a563bb5f58ebe37ac292ac1e
motopeco <peco@pelife.jp>
2022-08-31 10:25:09 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const login = async () => {
+    await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      credentials: 'include'
+    })
+
+    alert('login')
+  }
+
+  const logout = async () => {
+    await fetch('http://localhost:3000/logout', {
+      method: 'POST',
+      credentials: 'include'
+    })
+
+    alert('logout')
+  }
+
+  const connect = () => {
+    const ws = new WebSocket('ws://localhost:3000/websocket')
+  }
+
+  return (
+    <div className="App">
+      <button onClick={login}>ログイン</button>
+      <button onClick={logout}>ログアウト</button>
+      <button onClick={connect}>WebSocket</button>
+    </div>
+  )
+}
+
+export default App
